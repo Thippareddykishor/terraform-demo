afcced56a2a4423658757ccfa8d3b6531de49145
kishor <kishorebsc12@gmail.com>
2025-12-06 21:13:37 +0530

    added

--- learn-terraform/04-modules/main.tf
+++ learn-terraform/04-modules/main.tf
@@ -1,4 +1,8 @@
 module "null_resource" {
   source = "./null-res-module"
-  
+  }
+
+module "res-count" {
+  source = "./resource-count"
+  input = module.null_resource
 }
---
+++ learn-terraform/04-modules/resource-count/output.tf
@@ -0,0 +1,3 @@
+output "res-count" {
+  value = var.input
+}
